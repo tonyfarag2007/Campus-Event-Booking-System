bd64ca8cdb3bdd2241d0658665fb4dd942d91d0e
iamVunduv <alleksei5986@gmail.com>
2026-02-17 11:18:01 -0500

    Booking class

--- booking.java
+++ booking.java
@@ -14,36 +14,36 @@ public class booking{
         this.b_status = booking_status;
     }
     // Getters
-    public String getBId(){
+    public String get_book_id(){
         return this.b_id;
     }
-    public String getUId(){
+    public String get_user_id(){
         return this.u_id;
     }
-    public String getEId(){
+    public String get_event_id(){
         return this.e_id;
     }
-    public String getET(){
+    public String get_event_time(){
         return this.e_time;
     }
-    public String getBS(){
+    public String get_book_status(){
         return this.b_status;
     }
 
     // Setters
-    public void setBId(String booking_id){
+    public void set_book_id(String booking_id){
         this.b_id = booking_id;
     }
-    public void setUId(String user_id){
-        this.b_id = user_id;
+    public void set_user_id(String user_id){
+        this.u_id = user_id;
     }
-    public void setEId(String event_id){
-        this.b_id = event_id;
+    public void set_event_id(String event_id){
+        this.e_id = event_id;
     }
-    public void setET(String event_time){
-        this.b_time = e_time;
+    public void set_event_time(String event_time){
+        this.e_time = e_time;
     }
-    public void setBS(String booking_status){
+    public void set_book_status(String booking_status){
         this.b_status = booking_status;
     }
     
