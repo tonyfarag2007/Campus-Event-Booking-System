2a21d5fafbc06c6092e7e644004b79c5f4a0861d
nubb <sherm5344@gmail.com>
2026-02-20 08:15:36 -0500

    fix: added missing getters/setters and added ArrayList<String> for booking ids

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Event {
     /*
@@ -26,14 +27,17 @@ TODO: Should be able to dump confirmed/waitlisted users
     private int event_capacity;
     private String confirmed_users;
     private String waitlisted_users;
+    // Note that this is not initialized in the constructor. The Booking class will manipulate it accordingly.
+    private ArrayList<String> booking_ids;
 
     // EVENT SPECIFIC ATTRIBUTES, These may be implemented with inheritance if need be down the line!
     private String workshop_topic;
     private String seminar_speaker_name;
     // Note that age restriction is just for display and nothing more.
-    private int convert_age_restriction;
+    private int concert_age_restriction;
 
-    Event(String event_id, String event_title, Date event_date,
+    // Base constructor
+    public Event(String event_id, String event_title, Date event_date,
           String event_location, int event_capacity, String confirmed_users,
           String waitlisted_users) {
         this.event_id = event_id;
@@ -43,8 +47,11 @@ TODO: Should be able to dump confirmed/waitlisted users
         this.event_capacity = event_capacity;
         this.confirmed_users = confirmed_users;
         this.waitlisted_users = waitlisted_users;
+        this.booking_ids = new ArrayList<String>();
     }
-    Event(String event_id, String event_title, Date event_date,
+
+    // Workshop constructor
+    public Event(String event_id, String event_title, Date event_date,
           String event_location, int event_capacity, String confirmed_users,
           String waitlisted_users, String workshop_topic) {
         this.event_id = event_id;
@@ -55,8 +62,11 @@ TODO: Should be able to dump confirmed/waitlisted users
         this.confirmed_users = confirmed_users;
         this.waitlisted_users = waitlisted_users;
         this.workshop_topic = workshop_topic;
+        this.booking_ids = new ArrayList<String>();
     }
-    Event(String event_id, String event_title, Date event_date,
+
+    // Seminar constructor
+    public Event(String event_id, String event_title, Date event_date,
           String event_location, int event_capacity, String confirmed_users,
           String waitlisted_users, String seminar_speaker_name) {
         this.event_id = event_id;
@@ -67,10 +77,13 @@ TODO: Should be able to dump confirmed/waitlisted users
         this.confirmed_users = confirmed_users;
         this.waitlisted_users = waitlisted_users;
         this.seminar_speaker_name = seminar_speaker_name;
+        this.booking_ids = new ArrayList<String>();
     }
-    Event(String event_id, String event_title, Date event_date,
+
+    // Concert constructor
+    public Event(String event_id, String event_title, Date event_date,
           String event_location, int event_capacity, String confirmed_users,
-          String waitlisted_users, int convert_age_restriction) {
+          String waitlisted_users, int concert_age_restriction) {
         this.event_id = event_id;
         this.event_title = event_title;
         this.event_date = event_date;
@@ -78,6 +91,33 @@ TODO: Should be able to dump confirmed/waitlisted users
         this.event_capacity = event_capacity;
         this.confirmed_users = confirmed_users;
         this.waitlisted_users = waitlisted_users;
-        this.convert_age_restriction = convert_age_restriction;
+        this.concert_age_restriction = concert_age_restriction;
+        this.booking_ids = new ArrayList<String>();
     }
+
+    // getters
+    public String get_event_id() { return this.event_id; }
+    public String get_event_title() { return this.event_title; }
+    public Date get_event_date() { return this.event_date; }
+    public String get_event_location() { return this.event_location; }
+    public int get_event_capacity() { return this.event_capacity; }
+    public String get_confirmed_users() { return this.confirmed_users; }
+    public String get_waitlisted_users() { return this.waitlisted_users; }
+    public ArrayList<String> get_booking_ids() { return this.booking_ids; }
+    public String get_workshop_topic() { return this.workshop_topic; }
+    public String get_seminar_speaker_name() { return this.seminar_speaker_name; }
+    public int get_concert_age_restriction() { return this.concert_age_restriction; }
+
+    // setters
+    public String set_event_id(String new_event_id) { this.event_id = new_event_id; }
+    public String set_event_title(String new_event_title) { this.event_title = new_event_title; }
+    public Date get_event_date(Date new_event_date) { this.event_date = new_event_date; }
+    public String set_event_location(String new_event_location) { this.event_location = new_event_location; }
+    public int set_event_capacity(int new_event_capacity) { this.event_capacity = new_event_capacity; }
+    public String set_confirmed_users(String new_confirmed_users) { this.confirmed_users = new_confirmed_users; }
+    public String set_waitlisted_users(String new_waitlisted_users) { this.waitlisted_users = new_waitlisted_users; }
+    public ArrayList<String> set_booking_ids(ArrayList<String> new_booking_ids) { this.booking_ids = new_booking_ids; }
+    public String set_workshop_topic(String new_workshop_topic) { this.workshop_topic = new_workshop_topic; }
+    public String set_seminar_speaker_name(String new_seminar_speaker_name) { this.seminar_speaker_name = new_seminar_speaker_name; }
+    public int set_concert_age_restriction(int new_concert_age_restriction) { this.concert_age_restriction = new_concert_age_restriction; }
 }
